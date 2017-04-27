5c2cbb644de78a27b253595fe1a5f14b16c8ae78
Anastasia <popova.av27@gmail.com>
2017-04-27 12:13:16 +0300

    cubes location

--- cubes_segment.m
+++ cubes_segment.m
@@ -1,32 +1,19 @@
 function [c1,c2,c3] = cubes_segment(im)
 
-%im_name = '0022.bmp';
-
-%im = imread(im_name);
+%im_name = '0029.bmp';
+im = imread(im_name);
+min_area = 3000;
+max_area = 21000;
 
 [red_cube, blue_cube, green_cube] = hsv_segmentation(im,0.7,0.2);
 
-c1 = get_center(red_cube,10000,21000);
+c1 = get_center(red_cube,min_area,max_area);
+c2 = get_center(green_cube,min_area,max_area);
+c3 = get_center(blue_cube,min_area,max_area);
 
-%figure 
-%imshow(im);
-%title('Red cube')
-%hold on 
-%plot(c(1),c(2), 'r*');
-
-
-c2 = get_center(green_cube,10000,21000);
-
-%figure 
-%imshow(im);
-%title('Green cube')
-%hold on 
-%plot(c(1),c(2), 'r*');
-
-c3 = get_center(blue_cube,10000,21000);
 
 %figure 
 %imshow(im);
 %title('Blue cube')
 %hold on 
-%plot(c(1),c(2), 'r*');
+%plot([c1(1) c2(1) c3(1)],[c1(2) c2(2) c3(2)] , 'r*');
--- get_center.m
+++ get_center.m
@@ -1,13 +1,16 @@
 function centroid = get_center(img,min_area,max_area)
 stats=regionprops(img, 'ConvexHull','Centroid','Area','Solidity');
 
-cube_idx=find( [stats.Area] < max_area & [stats.Area] > min_area);
+cube_idx=find( [stats.Area] < max_area & [stats.Area] > min_area & [stats.Solidity] > 0.5);
 
-max_sol = 0;
+min_dist = 100000;
+center = size(img) / 2;
 centroid = [0 0];
+
 for i = 1:size(cube_idx,2);
-    if stats(cube_idx(i)).Solidity> max_sol
-        max_sol = stats(cube_idx(i)).Solidity;
+    dist = sqrt((center(2) - stats(cube_idx(i)).Centroid(1))^2 +  (center(1) - stats(cube_idx(i)).Centroid(2))^2);
+    if dist < min_dist
+        min_dist = dist;
         centroid = stats(cube_idx(i)).Centroid;
     end
 end
